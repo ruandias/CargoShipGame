74b467bf117079dd7c3fcbdb1df0d7766e4e5c1f
avanade.ruan <avanade.ruan@grupofleury.com.br>
2021-10-12 21:30:15 -0300

    removed default visual styles

--- WindowsGame/Program.cs
+++ WindowsGame/Program.cs
@@ -15,7 +15,6 @@ namespace WindowsGame
         static void Main()
         {
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
